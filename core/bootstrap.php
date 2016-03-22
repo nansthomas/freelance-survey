4b90461983c993c0396973f93e7a7a39b8ed59c9
Nans THOMAS <nans.thomas@hetic.net>
2016-03-22 23:05:30 +0100

    Push Starting Project

---
+++ core/bootstrap.php
@@ -0,0 +1,13 @@
+<?php
+
+require_once __DIR__ . '/config.inc.php';
+require_once __DIR__ . '/../vendor/autoload.php';
+
+use RedBean_Facade as R;
+
+if (empty(R::$currentDB)) {
+    $dsn = sprintf('%s:host=%s;dbname=%s', DB_TYPE, DB_HOST, DB_NAME);
+    R::setup($dsn, DB_USER, DB_PASSWORD);
+}
+
+R::close();
---
+++ core/config.inc.php
@@ -0,0 +1,13 @@
+<?php
+
+/** DATABASE **/
+
+define('DB_TYPE', 'mysql');
+define('DB_HOST', 'localhost');
+define('DB_NAME', 'freelancesurvey');
+define('DB_USER', 'root');
+define('DB_PASSWORD', 'root');
+
+/** REDBEAN CONFIG **/
+
+define('REDBEAN_MODEL_PREFIX', '\\freelanceSurvey\\Model');
