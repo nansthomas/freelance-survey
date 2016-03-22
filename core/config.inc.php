743e49f3110b090ba8ce268e67745ff4d4776946
Nans THOMAS <nans.thomas@hetic.net>
2016-03-22 23:27:03 +0100

    Fix Connection to bootstrap.php

--- core/config.inc.php
+++ core/config.inc.php
@@ -6,7 +6,7 @@ define('DB_TYPE', 'mysql');
 define('DB_HOST', 'localhost');
 define('DB_NAME', 'freelancesurvey');
 define('DB_USER', 'root');
-define('DB_PASSWORD', 'root');
+define('DB_PASSWORD', '');
 
 /** REDBEAN CONFIG **/
 
